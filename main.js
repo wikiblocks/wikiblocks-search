16a3cc56509b6a864bb787e42beb44af4a1138e0
Brooks Mershon <brooksmershon@gmail.com>
2015-11-14 23:17:14 -0500

    fixing detached head

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-app.use(logger('production'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
