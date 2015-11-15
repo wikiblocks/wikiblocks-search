15d11417f60dccf48ed48b632fea7ba48396fd47
Brooks Mershon <brooksmershon@gmail.com>
2015-11-14 21:17:18 -0500

    add back error handling and logging

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
 // modular routing
@@ -15,10 +15,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('production'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 app.use('/search', search);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.sendStatus(err.status || 500);
+});
 
 module.exports = app;
