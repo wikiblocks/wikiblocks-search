b2506c1469961481edb61122be2267b09bc49db6
Brooks Mershon <mershon.brooks@gmail.com>
2015-11-19 15:55:24 -0500

    discover endpoint

---
+++ create-tables.sql
@@ -0,0 +1,29 @@
+drop schema public cascade;
+create schema public;
+
+CREATE TABLE Gist
+(gistid VARCHAR(32) NOT NULL PRIMARY KEY,
+username VARCHAR(32) NOT NULL,
+description VarCHAR(256) NOT NULL);
+
+CREATE TABLE Tag
+(tag VARCHAR(256) NOT NULL UNIQUE,
+tagid SERIAL NOT NULL PRIMARY KEY); 
+
+CREATE TABLE Tag_Gist
+(gistid VARCHAR(64) NOT NULL REFERENCES Gist(gistid),
+tagid INT NOT NULL REFERENCES Tag(tagid), 
+PRIMARY KEY(gistid, tagid));
+
+CREATE TABLE Category
+(category VARCHAR(128) NOT NULL UNIQUE,
+categoryid SERIAL NOT NULL PRIMARY KEY);
+
+CREATE TABLE Tag_Category
+(categoryid INT NOT NULL REFERENCES Category(categoryid),
+tagid INT NOT NULL REFERENCES Tag(tagid));
+
+CREATE TABLE Category_Gist
+(gistid VARCHAR(64) NOT NULL REFERENCES Gist(gistid),
+categoryid INT NOT NULL REFERENCES Category(categoryid),
+PRIMARY KEY(gistid, categoryid));
---
+++ routes/discover.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var handler = require('../handlers/discover-handler.js');
+
+/* POST search object */
+router.post('/', function (req, res) {
+	var gist = req.body;
+
+	if(!gist.gistid || !gist.username || !gist.description) {
+    	return res.sendStatus(400);
+	}
+	// pass response to the middleware responsible for handling page
+	handler.handleGist(gist, res);
+});
+
+module.exports = router;
